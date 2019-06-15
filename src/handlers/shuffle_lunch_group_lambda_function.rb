91db8ba759d519d40fdad467c094f27539d8b3aa
tatematsu-k <ynu1264@gmail.com>
2019-06-16 08:29:42 +0900

    add rotation table

--- src/handlers/shuffle_lunch_group_lambda_function.rb
+++ src/handlers/shuffle_lunch_group_lambda_function.rb
@@ -1,7 +1,9 @@
 require 'json'
 require './src/models/user_table'
+require './src/services/shuffle_lunch_implementation_service'
 
 def lambda_handler(event:, context:)
+  ShuffleLunchImplementationService.new
   user = UserTable.new(email: "#{SecureRandom.uuid}@gmail.com")
   user.save!
   { statusCode: 200, body: JSON.generate({ user: user.to_h }) }
