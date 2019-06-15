3d79a5bc893f9f22088a6e2fce0bc8bedb4ef6f7
tatematsu-k <ynu1264@gmail.com>
2019-06-15 09:03:51 +0900

    add profile to deployment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,2 @@
-sam package --template-file template.yaml --output-template-file packaged-template.yaml --s3-bucket shuffle-lunch-deployment
-sam deploy --template-file packaged-template.yaml --stack-name shuffleLunchDynamo --capabilities CAPABILITY_IAM
+sam package --template-file template.yaml --output-template-file packaged-template.yaml --s3-bucket shuffle-lunch-deployment --profile sandbox
+sam deploy --template-file packaged-template.yaml --stack-name shuffleLunchDynamo --capabilities CAPABILITY_IAM --profile sandbox
