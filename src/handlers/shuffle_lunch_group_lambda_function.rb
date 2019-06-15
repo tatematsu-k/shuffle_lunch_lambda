dbcf74318d61baa1a39471d101257570793cc24d
tatematsu-k <ynu1264@gmail.com>
2019-06-15 15:47:08 +0900

    refix for user_table

--- src/handlers/shuffle_lunch_group_lambda_function.rb
+++ src/handlers/shuffle_lunch_group_lambda_function.rb
@@ -2,7 +2,7 @@ require 'json'
 require './src/models/user_table'
 
 def lambda_handler(event:, context:)
-  user = DemoTable.new(email: "#{SecureRandom.uuid}@gmail.com")
+  user = UserTable.new(email: "#{SecureRandom.uuid}@gmail.com")
   user.save!
-  { statusCode: 200, body: JSON.generate({ user: user.to_hash }) }
+  { statusCode: 200, body: JSON.generate({ user: user.to_h }) }
 end
--- src/models/user_table.rb
+++ src/models/user_table.rb
@@ -3,6 +3,5 @@ require 'aws-record'
 class UserTable
   include Aws::Record
   set_table_name ENV['DDB_TABLE']
-  string_attr :id, hash_key: true
-  string_attr :body
+  string_attr :email, hash_key: true
 end
