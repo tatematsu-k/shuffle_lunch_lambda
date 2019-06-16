eff168555356657e1835fdbb9b3f4a3644f61f10
tatematsu-k <ynu1264@gmail.com>
2019-06-16 17:59:24 +0900

    remove implementation table

--- src/models/candidate_table.rb
+++ src/models/candidate_table.rb
@@ -3,6 +3,7 @@ require 'aws-record'
 class CandidateTable
   include Aws::Record
   set_table_name ENV['CANDIDATE_TABLE']
-  integer_attr :rotation_id, hash_key: true
+  integer_attr :rotation_id, hash_key: true, default_value: 1
   string_attr :user_email
+  string_attr :lunch_group, default_value: "not_assigned"
 end
--- src/models/implementation_table.rb
+++
@@ -1,9 +0,0 @@
-require 'aws-record'
-
-class ImplementationTable
-  include Aws::Record
-  set_table_name ENV['IMPLEMENTATION_TABLE']
-  string_attr :rotation_id, hash_key: true
-  string_attr :shuffle_lunch_group_id
-  string_attr :user_email
-end
