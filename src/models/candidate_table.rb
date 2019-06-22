c8b0c7a3dec8c2d4100ed1b0f8af7467eeaf0e11
tatematsu-k <ynu1264@gmail.com>
2019-06-22 12:34:37 +0900

    add info for candidate at post single candidate

--- src/models/candidate_table.rb
+++ src/models/candidate_table.rb
@@ -6,4 +6,5 @@ class CandidateTable
   string_attr :lunch_group, default_value: "not_assigned"
   integer_attr :rotation_id, hash_key: true, default_value: 1
   string_attr :user_email, range_key: true
+  map_attr :info
 end
