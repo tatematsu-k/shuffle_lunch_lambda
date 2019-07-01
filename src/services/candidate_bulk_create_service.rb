8dbe6464e6f7a90d8fe93ac0c18dfe80ab46ab37
tatematsu-k <ynu1264@gmail.com>
2019-07-02 08:54:53 +0900

    add lunch_group name

--- src/services/candidate_bulk_create_service.rb
+++ src/services/candidate_bulk_create_service.rb
@@ -3,7 +3,7 @@ require './src/models/candidate_table'
 class CandidateBulkCreateService
   def self.bulk_create!(candidates_param)
     candidates_param.each do |param|
-      candidate = CandidateTable.new(param.slice(:rotation_id, :user_email, :user_name, :info))
+      candidate = CandidateTable.new(param.slice(:rotation_id, :user_email, :user_name, :info, :lunch_group))
       candidate.save!
     end
     true
